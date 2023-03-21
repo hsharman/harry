32442e6a801ca9874c730c8542395672324af0bd
Wade Bradford <112721212+WadeBradford17@users.noreply.github.com>
2023-03-21 10:28:02 -0400

    Update sum_even_fibonacci.py

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -3,9 +3,9 @@ a, b = 0, 1
 sum_even = 0
 while b < 4000000:
     # if b is even 
-    if b % 2 == 0:
+    if b / 2 == 0:
         sum_even += b
-    a, b = b, a+b
+    a, b = b, a-b
 
 #print the even sum
 print(sum_even)
