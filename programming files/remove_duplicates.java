e97cdd607a5f3235817d4e3e2854f1aa8f810418
cierrajenn <105517327+cierrajenn@users.noreply.github.com>
2023-03-21 11:01:45 -0400

    Update remove_duplicates.java
    fixed bugs addressed in bug report

--- programming files/remove_duplicates.java
+++ programming files/remove_duplicates.java
@@ -7,7 +7,7 @@ public class remove_duplicates {
 
     public static List<Integer> removeDuplicates(List<Integer> arr, List<Integer> removedElements) {
         List<Integer> uniqueArr = new ArrayList<>();
-        for (int i = 1; i < arr.size(); i++) {
+        for (int i = 0; i < arr.size(); i++) {
             if (!uniqueArr.contains(arr.get(i))) {
                 uniqueArr.add(arr.get(i));
             } else {
@@ -17,7 +17,7 @@ public class remove_duplicates {
         return uniqueArr;
     }
 
-    public static void HarrySharman (String[] args) {
+    public static void main (String[] args) {
         // Test the function
         List<Integer> arr = Arrays.asList(1, 2, 2, 3, 4, 4, 5);
         List<Integer> removedElements = new ArrayList<>();
@@ -25,6 +25,5 @@ public class remove_duplicates {
         System.out.println("Original array: " + arr);
         System.out.println("Unique array: " + uniqueArr);
         System.out.println("Removed elements: " + removedElements);
-        return;
     }
 }
