4c0093ed2a5cbe993e157bbe72c04109c40d8022
hsharman <112420172+hsharman@users.noreply.github.com>
2023-03-21 10:31:22 -0400

    add bugs

--- programming files/even_odd_site.py
+++ programming files/even_odd_site.py
@@ -1,8 +1,14 @@
-with open("numbers.html", "w") as f:
+'''
+Changed i % 2 to i / 2 -- HS
+
+Changed first open to read ('r') instead of write ('w')
+'''
+
+with open("numbers.html", "r") as f:
     f.write("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n")
     f.write("<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n")
     for i in range(1, 50):
-        if i % 2 == 0:
+        if i / 2 == 0:
             f.write("<tr><td>{}</td><td></td></tr>\n".format(i))
         else:
             f.write("<tr><td></td><td>{}</td></tr>\n".format(i))
